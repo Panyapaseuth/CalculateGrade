dccd3ad20cbb914f7c79e7d350653ea9d497bb7e
Panyapaseuth <54263907+Panyapaseuth@users.noreply.github.com>
2021-08-18 16:34:48 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        try {
+            File a = new File("Score.txt");
+        }catch (Exception e){
+
+        }
+    }
+}
