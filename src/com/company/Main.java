634ff4ed49930e8079edd8318c878c7897e7c34d
Panyapaseuth <54263907+Panyapaseuth@users.noreply.github.com>
2021-08-19 13:50:14 +0700

    Input & Output File java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,55 @@
 package com.company;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        PrintWriter output = null;
         try {
             File a = new File("Score.txt");
-        }catch (Exception e){
+            output = new PrintWriter(new File("Grade.txt"));
+            Scanner input = new Scanner(a);
 
+            String nisit = "";
+
+            while (input.hasNext()){
+                nisit = input.nextLine();
+//                System.out.println("Information = " + nisit);
+                int pos = nisit.indexOf(" ");
+                String scoreStr = nisit.substring(pos, nisit.length());
+
+//                System.out.println(pos);
+//                System.out.println(score);
+//                System.out.println(scoreStr.trim());
+
+                scoreStr = scoreStr.trim();
+                double score = Double.parseDouble(scoreStr); //Change String to integer
+
+                String grade = "";
+
+                if(score>80){
+                    grade = "A";
+                }else if(score>70){
+                    grade = "B";
+                }else if(score>60){
+                    grade = "C";
+                }else if(score>50){
+                    grade = "D";
+                }else {
+                    grade = "F";
+                }
+
+                System.out.println(nisit + " = " + grade);
+                output.println(nisit + " = " + grade);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        output.close();
     }
 }
